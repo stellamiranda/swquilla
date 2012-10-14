bd2e2f70edb83de733464ae7676c443383786fb9
Pedro Fernandez <pedro.fernandez@koombea.com>
2012-10-14 15:39:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swquilla::Application.config.secret_token = '412eefdd294d8c4b0de5cc35a884f5854b0e6ed8f3f59d557b41d2e51cf5b9fe3ade635b1ff26394b775fa9905f192a88a7348f8064b7a7f6c0b04a02a80384b'
